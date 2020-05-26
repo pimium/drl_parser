ced48a6dc1b0953957a46591bdf4a617d6a718bc
Olivier Pimi <pimium@gmail.com>
2020-05-26 07:05:37 +0200

    add adjustment

--- drill_group.py
+++ drill_group.py
@@ -4,7 +4,7 @@ class DrillGroup:
 	def __init__(self, file=None, number=None, diameter=None):
 
 		self.file = file
-		self.number=number
+		self.number = number
 		self.diameter = diameter
 		self.drill = []
 
@@ -26,12 +26,12 @@ class DrillGroup:
 					break
 				if (start_reading == 0):
 					if (re.match('T'+self.number+'\s', line)):	
-						print line,
+						print(line),
 						start_reading = 1
 				elif start_reading:
 					if (re.match('T\d\s', line)):
-						print "\nEXIT\n",
-						print line
+						print("\nEXIT\n"),
+						print(line)
 						stop_reading = 1
 						break
 					else:
@@ -39,8 +39,8 @@ class DrillGroup:
 							x = re.search('[X]([+-]?\d*.\d*)', line)
 							y = re.search('[Y]([+-]?\d*.\d*)', line)
 							if x:
-								x = x.group(1) 
+								x = float(x.group(1))
 							if y:
-								y = y.group(1) 
-							self.drill.append((x,y))
-							
+								y = float(y.group(1))
+							self.drill.append((x, y))
+
--- drl_parser.py
+++ drl_parser.py
@@ -1,117 +1,150 @@
 import re
 import json
 import argparse
-from drill_group import DrillGroup 
+
+from decimal import Decimal
+
+from drill_group import DrillGroup
+
 
 class SetEncoder(json.JSONEncoder):
-	def default(self, obj):
-		if isinstance(obj, DrillParser):
-			return obj.__str__()
-		if isinstance(obj, DrillGroup):
-			return 'CustomSomethingRepresentation'
-		return json.JSONEncoder.default(self, obj)
+    def default(self, obj):
+        if isinstance(obj, DrillParser):
+            return obj.__str__()
+        if isinstance(obj, DrillGroup):
+            return 'CustomSomethingRepresentation'
+        return json.JSONEncoder.default(self, obj)
+
 
 class DrillParserEncoder(json.JSONEncoder):
-	def default(self, obj):
-		dict = {'DrillParser': 'DrillGroups'}
-		if isinstance(obj, DrillParser):
-			try:
-				iterable = iter(obj.drillgroup)
-			except TypeError:
-				pass
-			else:
-				# dict['DrillParser'] = iterable
-				return {'data': list(iterable)}
-			# return json.JSONEncoder.default(self, obj)
+    def default(self, obj):
+        dict = {'DrillParser': 'DrillGroups'}
+        if isinstance(obj, DrillParser):
+            try:
+                iterable = iter(obj.drillgroup)
+            except TypeError:
+                pass
+            else:
+                # dict['DrillParser'] = iterable
+                return {'data': list(iterable)}
+        # return json.JSONEncoder.default(self, obj)
 
-		if isinstance(obj, DrillGroup):
-			ret_list = []
-			
-			try:
-				iteraable = iter(obj.drill)
-			except TypeError:
-				pass
-			else:
-				for items in iteraable:
-					ret_list.append([items[0], items[1]])
-				return {'number':obj.number, 'diameter':obj.diameter, 'drill': list(ret_list)}
-				# return list(iterable)
-				
-		return json.JSONEncoder.default(self, obj)
+        if isinstance(obj, DrillGroup):
+            ret_list = []
+
+            try:
+                iteraable = iter(obj.drill)
+            except TypeError:
+                pass
+            else:
+                for items in iteraable:
+                    ret_list.append([items[0], items[1]])
+                return {'number': obj.number, 'diameter': obj.diameter, 'drill': list(ret_list)}
+            # return list(iterable)
+
+        return json.JSONEncoder.default(self, obj)
+
 
 def getParmeters():
+    parser = argparse.ArgumentParser(description="Script to parse drl-files")
+    parser.add_argument("-V", "--verbose", action="store_true", default=False, help="verbose")
 
-	parser = argparse.ArgumentParser(version="1.0", description="Script to parse drl-files")
-	parser.add_argument("-V", "--verbose", action="store_true", default=False, help="verbose")
-	
-	parser.add_argument("-if","--inputfile", action="store", default="",  dest="inputfile", help="input filepath")
-	
-	parser.add_argument("-of","--outputfile", action="store", default="", dest="outputfile", help="output filepath")
-		
-	args = parser.parse_args()
+    parser.add_argument("-if", "--inputfile", action="store", default="", dest="inputfile", help="input filepath")
 
-	return args
+    parser.add_argument("-of", "--outputfile", action="store", default="", dest="outputfile", help="output filepath")
+
+    args = parser.parse_args()
+
+    return args
+
 
 class DrillParser:
-	def __init__(self, drillgroup=[], inputfile="drl_test.drl", outputfile="", config=getParmeters()):
+    def __init__(self, drillgroup=[], inputfile="drl_test.drl", outputfile="", config=getParmeters()):
 
-		self.drillgroup = drillgroup
-		self.outputfile = outputfile
-		self.inputfile  = inputfile
-		self.config = config
-		
-		if config != None :
-			if(config.inputfile):
-				self.inputfile = config.inputfile
+        self.drillgroup = drillgroup
+        self.outputfile = outputfile
+        self.inputfile = inputfile
+        self.config = config
 
-			if(config.outputfile):
-				self.outputfile = config.outputfile
+        if config != None:
+            if (config.inputfile):
+                self.inputfile = config.inputfile
 
-		if self.outputfile == "":
-			self.outputfile = self.inputfile + ".js"
+            if (config.outputfile):
+                self.outputfile = config.outputfile
 
+        if self.outputfile == "":
+            self.outputfile = self.inputfile + ".js"
 
-	def __str__(self):
-		test = '{ "data": \n['
-		for items in self.drillgroup:
-			test = test + items.__str__()
-		test = test + ']}\n'
-		return test
+    def __str__(self):
+        test = '{ "data": \n['
+        for items in self.drillgroup:
+            test = test + items.__str__()
+        test = test + ']}\n'
+        return test
 
-	def writeJsonToFile(self):
-		try:			
-			with open(self.outputfile, 'w') as f:
-				try:
-					json.dump(self, f, cls=DrillParserEncoder, indent=4)
-					#f.write(self.results)
-				except Exception as e:
-					print "Could not write data to %s" %(self.outputfile)
-		except IOError:
-			print "Could not open file ", self.outputfile
+    def writeJsonToFile(self):
+        try:
+            with open(self.outputfile, 'w') as f:
+                try:
+                    json.dump(self, f, cls=DrillParserEncoder, indent=4)
+                # f.write(self.results)
+                except Exception as e:
+                    print("Could not write data to %s" % (self.outputfile))
+        except IOError:
+            print("Could not open file ", self.outputfile)
+
+    def get_small_coodinate(self):
+        x = None
+        y = None
+
+        for items in self.drillgroup:
+            for drill in items.drill:
+                if not x != None:
+                    x = drill[0]
+                elif x > drill[0]:
+                    x = drill[0]
+
+                if not y != None:
+                    y = (drill[1])
+                elif y > (drill[1]):
+                    y = (drill[1])
+        return x, y
+
+    def adjust_coordinate(self):
+        x_min, y_min = self.get_small_coodinate()
+        for items in self.drillgroup:
+            new_dg = []
+            for drill in items.drill:
+                new_dg.append((drill[0] - x_min, drill[1] - y_min))
+            items.drill = new_dg
 
 def main():
-	obj = DrillParser()
-	with open(obj.inputfile, "r") as file:
-		for line in file: 
-			if (re.match('^[T]\d+C\d*.\d*', line)):
-				m = re.search('^[T](.+?)C', line)
-				if m:
-					found = m.group(1)
-					# print "Found: " + found
-					dg = DrillGroup(obj.inputfile, found)
-					dg.diameter = re.findall(r'C(\d*\.\d*)?', line)[0]
-					obj.drillgroup.append(dg)
-				# print line,
-	print obj
-	for items in obj.drillgroup:
-		print len(items.drill)
-		items.parse()
+    obj = DrillParser()
+    with open(obj.inputfile, "r") as file:
+        for line in file:
+            if (re.match('^[T]\d+C\d*.\d*', line)):
+                m = re.search('^[T](.+?)C', line)
+                if m:
+                    found = m.group(1)
+                    # print "Found: " + found
+                    dg = DrillGroup(obj.inputfile, found)
+                    dg.diameter = re.findall(r'C(\d*\.\d*)?', line)[0]
+                    obj.drillgroup.append(dg)
+            # print line,
+    print(obj)
+    for items in obj.drillgroup:
+        print(len(items.drill))
+        items.parse()
 
-	for items in obj.drillgroup:
-		print len(items.drill)
+    obj.adjust_coordinate()
 
-	print json.dumps(obj.drillgroup[0], cls=DrillParserEncoder, indent=4) 
-	obj.writeJsonToFile() 
+
+# for items in obj.drillgroup:
+#	print(len(items.drill))
+
+# print(json.dumps(obj.drillgroup[0], cls=DrillParserEncoder, indent=4))
+# obj.writeJsonToFile()
 
 if __name__ == '__main__':
-	main()
+    main()
